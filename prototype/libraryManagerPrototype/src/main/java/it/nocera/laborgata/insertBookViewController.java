877f66a53dc4a2fc0794fef3a32c66b4323990f4
laywork22 <flytrout2@hotmail.com>
2025-12-01 18:30:32 +0100

    Aggiunti i 3 file FXML della UI e due controller associati rispettivamente a insertBookView.fxml e insertLoanView.fxml al prototipo

---
+++ prototype/libraryManagerPrototype/src/main/java/it/nocera/laborgata/insertBookViewController.java
@@ -0,0 +1,14 @@
+package it.nocera.laborgata;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class insertBookViewController {
+    @FXML
+    private AnchorPane insertBookContent;
+
+    @FXML
+    public void initialize() {
+    }
+
+}
---
+++ prototype/libraryManagerPrototype/src/main/java/it/nocera/laborgata/insertLoanViewController.java
@@ -0,0 +1,4 @@
+package it.nocera.laborgata;
+
+public class insertLoanViewController {
+}
