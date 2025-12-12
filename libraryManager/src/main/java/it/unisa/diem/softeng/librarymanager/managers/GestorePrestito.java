eed24e74768c7eae777c65f29dd703e7c417811d
dursoantonio <dursoantonio0@gmail.com>
2025-12-12 15:19:12 +0100

    correzione bug

--- libraryManager/src/main/java/it/unisa/diem/softeng/librarymanager/managers/GestorePrestito.java
+++ libraryManager/src/main/java/it/unisa/diem/softeng/librarymanager/managers/GestorePrestito.java
@@ -43,10 +43,10 @@ public class GestorePrestito implements Gestore<Prestito>{
             if (l.getLibro().getCopieDisponibili() <= 0) {
                 throw new PrestitoException("Non ci sono copie disponibili per il libro: " + l.getLibro().getTitolo());
             }
-            if(l.getLibro().getAttivo()){
+            if(!l.getLibro().getAttivo()){
                 throw new PrestitoException("Il libro risulta non attivo");
             }
-            if(l.getUtente().getAttivo()){
+            if(!l.getUtente().getAttivo()){
                 throw new PrestitoException("L'utente risulta non attivo");
             }
             l.getUtente().setCountPrestiti(l.getUtente().getCountPrestiti()+1);
